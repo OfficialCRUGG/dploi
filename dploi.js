5b09280e9fa2e97531539cba03fc49c9c6b966f1
Ron31 <ron3108@gmx.net>
2019-10-01 19:06:35 +0200

    Revert ":wrench: Commands to Array"
    This reverts commit b08950399b9395b3250734cf301b0558964081e3.

--- dploi.js
+++ dploi.js
@@ -7,7 +7,6 @@ const services = require("./services.json");
 const commands = require("./commands.json");
 const { WebhookClient, RichEmbed } = require("discord.js");
 const { exec } = require('child_process');
-const { replace } = require("./utils/commandReplace");
 app.use(bodyparser.json({ type: "application/json" }));
 app.use(bodyparser.urlencoded({ extended: true }));
 
@@ -21,16 +20,16 @@ app.get("/autoDeploy/:app", (req, res) => {
     res.send({status: "error", code: 2, message: "This is a GET Request, you need to use a POST Request however."});
 });
 app.post("/autoDeploy/:app", (req, res) => {
-    services.forEach( async (service) => {
-        if (req.params.app  == service.url) {
+    services.forEach((service) => {
+        if(req.params.app == service.url) {
             let startTime = new Date();
-            if (service.webhooks.discord) {
+            if(service.webhooks.discord) {
                 let embed = new RichEmbed()
-                    .setTitle(req.body.repository.full_name)
-                    .setDescription("Deploying `" + req.body.repository.full_name + "`...")
-                    .setFooter(req.body.head_commit.author.username)
-                    .setColor("#FFE100")
-                    .setTimestamp(Date.now());
+                .setTitle(req.body.repository.full_name)
+                .setDescription("Deploying `" + req.body.repository.full_name + "`...")
+                .setFooter(req.body.head_commit.author.username)
+                .setColor("#FFE100")
+                .setTimestamp(Date.now());
                 req.body.commits.forEach((commit) => {
                     embed.addField(commit.id.substring(0, 7), commit.message);
                 });
@@ -38,41 +37,37 @@ app.post("/autoDeploy/:app", (req, res) => {
                 webhook.send(embed);
                 webhook.destroy();
             }
-            let commandsToExec = replace(commands[service.action.type], service.action.variables);
-            await commandsToExec.forEach((element) => {
-                exec(element, (error, stdout, stderror) => {
-                    if (error) {
-                        if (service.webhooks.discord) {
-                            let embed = new RichEmbed()
-                                .setTitle(req.body.repository.full_name)
-                                .setDescription("Error occured whilst deploying `" + req.body.repository.full_name + "`...")
-                                .setFooter(req.body.head_commit.author.username)
-                                .setColor("#FF2929")
-                                .setTimestamp(Date.now());
-                            let webhook = new WebhookClient(service.webhooks.discord.split("/")[5], service.webhooks.discord.split("/")[6]);
-                            webhook.send(embed);
-                            webhook.destroy();
-                            return;
-                        }
+            exec(commands[service.action.type].replace("%dir%", service.action.directory).replace("%pm2name%", service.action.pm2name), (error, stdout, stderror) => {
+                if(error) {
+                    if(service.webhooks.discord) {
+                        let embed = new RichEmbed()
+                        .setTitle(req.body.repository.full_name)
+                        .setDescription("Error occured whilst deploying `" + req.body.repository.full_name + "`...")
+                        .setFooter(req.body.head_commit.author.username)
+                        .setColor("#FF2929")
+                        .setTimestamp(Date.now());
+                        let webhook = new WebhookClient(service.webhooks.discord.split("/")[5], service.webhooks.discord.split("/")[6]);
+                        webhook.send(embed);
+                        webhook.destroy();
+                        return;
                     }
-                })
-            });
-
-            let endTime = new Date();
-            if (service.webhooks.discord) {
-                let embed = new RichEmbed()
+                }
+                let endTime = new Date();
+                if(service.webhooks.discord) {
+                    let embed = new RichEmbed()
                     .setTitle(req.body.repository.full_name)
                     .setDescription("Successfully deployed `" + req.body.repository.full_name + "` in " + (startTime - endTime) + "ms.")
                     .setFooter(req.body.head_commit.author.username)
                     .setColor("#87FF44")
                     .setTimestamp(Date.now());
-                let webhook = new WebhookClient(service.webhooks.discord.split("/")[5], service.webhooks.discord.split("/")[6]);
-                webhook.send(embed);
-                webhook.destroy();
-            }
+                    let webhook = new WebhookClient(service.webhooks.discord.split("/")[5], service.webhooks.discord.split("/")[6]);
+                    webhook.send(embed);
+                    webhook.destroy();
+                }
+            });
         }
-    })
-})
+    });
+});
 
 let server = app.listen(process.env.EXPRESS_PORT, process.env.EXPRESS_IP, () => {
     log.info("Express is now running on " + process.env.EXPRESS_IP + ":" + process.env.EXPRESS_PORT);
