0e250a5762c18acb77c4379b02169bc65cf82610
CRUGG <ich@crugg.de>
2019-10-01 19:15:37 +0200

    :recycle: Changed some minor things

--- utils/commandReplace.js
+++ utils/commandReplace.js
@@ -1,9 +1,9 @@
 module.exports.replace = (commands, variables) => {
     let commandsToRun = [];
-    for (let cmd in commands) {
-        if (commands.hasOwnProperty(cmd)) {
+    for(let cmd in commands) {
+        if(commands.hasOwnProperty(cmd)) {
             let command = commands[cmd];
-            if (command.match(/\${+[0-9]+}/gm)) {
+            if(command.match(/\${+[0-9]+}/gm)) {
                 let cmdNum = command.match(/\${+[0-9]+}/gm)[0].split('${')[1].split('}')[0];
                 command = command.replace(/\${+[0-9]+}/gm, variables[cmdNum]);
                 commandsToRun.push(command);
